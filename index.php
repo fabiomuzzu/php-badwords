161536f12c666cb2f22e6f9b784f2e806361d422
fabiomuzzu <fabio.muzzu@virgilio.it>
2024-01-23 16:37:35 +0100

    [ADD] bootstrap cdn, starting form

--- index.php
+++ index.php
@@ -3,10 +3,42 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="./css/style.css">
     <title>Document</title>
 </head>
 <body>
-    
+
+    <!-- HEADER -->
+    <header>
+        <div class="container">
+            <div class="row">
+                <div class="col-12 text-center my-3">
+                    <h1>Badwords</h1>
+                </div>
+            </div>
+        </div>
+    </header>
+
+    <!-- MAIN -->
+    <main>
+        <div class="container">
+            <div class="row">
+                <form action="">
+                    <div class="col-6 py-3">
+                        <label for="control-label">Paragrafo</label>
+                        <input class="form-control" type="text" >
+                    </div>
+                    <div class="col-6 py-3">
+                        <label for="control-label">Badword</label>
+                        <input class="form-control" type="text">
+                    </div>
+                    <div class="col-12">
+
+                    </div>
+                </form>
+            </div>
+        </div>
+    </main>
 </body>
 </html>
